2b556f8af0a86b9c607aa5de59a4b993797b395b
miguelesco <miuelangelescorche@gmail.com>
2021-08-09 17:03:15 -0400

    modified config structure

---
+++ config/webpack.dev.js
@@ -0,0 +1,34 @@
+const { default: merge } = require("webpack-merge");
+const common = require("./webpack.common");
+
+const scssRules = {
+  test: /\.s[ac]ss$/i,
+  use: [
+    // Creates `style` nodes from JS strings
+    'style-loader',
+    // Translates CSS into CommonJS
+    'css-loader',
+    // Compiles Sass to CSS
+    'sass-loader',
+  ],
+};
+
+/** @type {import('webpack').Configuration} **/
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 3000,
+    contentBase: '../dist',
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    open: true
+  },
+  target: "web",
+  devtool: "eval-source-map",
+  module: {
+    rules: [scssRules]
+  }
+}
+
+module.exports = merge(common, devConfig)
---
+++ config/webpack.prod.js
@@ -0,0 +1,34 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { default: merge } = require("webpack-merge");
+const common = require("./webpack.common");
+
+const scssRules = {
+  test: /\.s[ac]ss$/i,
+  use: [
+    // Creates `style` nodes from JS strings
+    MiniCssExtractPlugin.loader,
+    // Translates CSS into CommonJS
+    'css-loader',
+    // Compiles Sass to CSS
+    'sass-loader',
+  ],
+};
+
+/** @type {import('webpack').Configuration} **/
+const prodConfig = {
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    }
+  },
+  plugins: [
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [scssRules]
+  }
+}
+
+module.exports = merge(common, prodConfig)
